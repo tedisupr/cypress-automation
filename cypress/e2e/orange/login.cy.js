a5d03725ae8432b8356d0885cb3b4d408fded00f
tedisupr <tedi.suprianto@gmail.com>
2023-06-02 17:45:04 +0700

    login

--- cypress/e2e/orange/login.cy.js
+++ cypress/e2e/orange/login.cy.js
@@ -1,5 +1,27 @@
-describe('template spec', () => {
-  it('login', () => {
-    cy.visit('https://opensource-demo.orangehrmlive.com/')
+describe('Login Functionality', () => {
+  it('login_berhasil', () => {
+    cy.visit('https://www.saucedemo.com/')
+    cy.get('[data-test="username"]').type('standard_user')
+    cy.get('[data-test="password"]').type('secret_sauce')
+    cy.get('[data-test="login-button"]').click()
+    cy.get('.app_logo').should('be.visible')
+    cy.get('.title').should('be.visible')
+    cy.url().should('include', '/inventory.html')
+  })
+
+  it('password_salah',() =>{
+    cy.visit('https://www.saucedemo.com/')
+    cy.get('[data-test="username"]').type('standard_user')
+    cy.get('[data-test="password"]').type('secret_sauc')
+    cy.get('[data-test="login-button"]').click()
+    cy.get('[data-test="error"]').should('be.visible')
+  })
+
+  it('username_salah',() =>{
+    cy.visit('https://www.saucedemo.com/')
+    cy.get('[data-test="username"]').type('standard_use')
+    cy.get('[data-test="password"]').type('secret_sauce')
+    cy.get('[data-test="login-button"]').click()
+    cy.get('[data-test="error"]').should('be.visible')
   })
 })
