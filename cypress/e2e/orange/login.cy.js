6a2ed384b954974ff255e7de1feba15ad537716c
tedisupr <tedi.suprianto@gmail.com>
2023-06-02 18:38:02 +0700

    login

--- cypress/e2e/orange/login.cy.js
+++ cypress/e2e/orange/login.cy.js
@@ -1,27 +1,34 @@
 describe('Login Functionality', () => {
   it('login_berhasil', () => {
-    cy.visit('https://www.saucedemo.com/')
-    cy.get('[data-test="username"]').type('standard_user')
-    cy.get('[data-test="password"]').type('secret_sauce')
-    cy.get('[data-test="login-button"]').click()
-    cy.get('.app_logo').should('be.visible')
-    cy.get('.title').should('be.visible')
-    cy.url().should('include', '/inventory.html')
+    cy.visit('https://opensource-demo.orangehrmlive.com/')
+    cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type('Admin')
+    cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type('admin123')
+    cy.get('.oxd-button').click()
+    cy.get('.oxd-topbar-header-breadcrumb > .oxd-text').should('be.visible')
+    cy.get('.oxd-userdropdown-tab').should('be.visible')
+    cy.get(':nth-child(8) > .oxd-main-menu-item').should('be.visible')
+    cy.url().should('include', 'web/index.php/dashboard/index')
   })
 
-  it('password_salah',() =>{
-    cy.visit('https://www.saucedemo.com/')
-    cy.get('[data-test="username"]').type('standard_user')
-    cy.get('[data-test="password"]').type('secret_sauc')
-    cy.get('[data-test="login-button"]').click()
-    cy.get('[data-test="error"]').should('be.visible')
+  it('username_salah', () => {
+    cy.visit('https://opensource-demo.orangehrmlive.com/')
+    cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type('Admi')
+    cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type('admin123')
+    cy.get('.oxd-button').click()
+    cy.get('.oxd-alert-content > .oxd-text').should('be.visible')
   })
 
-  it('username_salah',() =>{
-    cy.visit('https://www.saucedemo.com/')
-    cy.get('[data-test="username"]').type('standard_use')
-    cy.get('[data-test="password"]').type('secret_sauce')
-    cy.get('[data-test="login-button"]').click()
-    cy.get('[data-test="error"]').should('be.visible')
+  it('password_salah', () => {
+    cy.visit('https://opensource-demo.orangehrmlive.com/')
+    cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type('Admin')
+    cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type('admin12')
+    cy.get('.oxd-button').click()
+    cy.get('.oxd-alert-content > .oxd-text').should('be.visible')
+  })
+
+  it('password_salah', () => {
+    cy.visit('https://opensource-demo.orangehrmlive.com/')
+    cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').should('be.visible')
+    cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').should('be.visible')
   })
 })
